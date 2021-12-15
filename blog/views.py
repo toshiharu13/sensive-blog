5808ce50181e0895bee37f7ff6b1926d207e4f2e
Boyko Alexey <elorian@yandex.ru>
2021-12-15 15:45:51 +0300

    fork serialize_post -> serialize_post_optimized

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,25 @@ from blog.models import Comment, Post, Tag
 from django.db.models import Count
 
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
+'''def get_related_posts_count(tag):
+    return tag.posts.count()'''
 
 
 def serialize_post(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': len(Comment.objects.filter(post=post)),
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
+def serialize_post_optimized(post):
     return {
         'title': post.title,
         'teaser_text': post.text[:200],
