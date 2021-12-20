2ecab98fbe297316eaf24c6a6a8e16df5ce2f725
elf <elorian@yandex.ru>
2021-12-20 23:16:22 +0300

    starting clenup code in post_detail

--- blog/views.py
+++ blog/views.py
@@ -93,7 +93,7 @@ def post_detail(request, slug):
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': len(likes),
+        'likes_amount': likes.count(),
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
